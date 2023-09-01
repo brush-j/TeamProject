ffbc38eef787d63f4fbd1300187fc5fb4d959b79
brush-j <parefaro@gmail.com>
2023-09-01 10:37:04 +0900

    #5 애플리케이션 실행
    1. Spring Boot Project 생성하기
    2. 빌드 도구 설정(메이븐)
    3. 설정 파일(application.properties)
    4. Lombok 라이브러리 적용하기

---
+++ homeshopping/src/test/java/namkyu/and/mz/homeshopping/HomeshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package namkyu.and.mz.homeshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
